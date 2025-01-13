0c16052259f5b89332a21c30b5d97bfc1de16309
Daniel Arevalos <daniel.arevalos@sansano.usm.cl>
2025-01-13 17:36:12 +0100

    small changes

--- sstadex/mna/mna.py
+++ sstadex/mna/mna.py
@@ -5,7 +5,9 @@ import sympy as sym
 
 from sstadex.utils import spice2ptxt
 
+
 def mna(XSCHEM_RCFILE, XSCHEM_DIR, SPICE_DIR, OUTPUT_DIR, macromodel):
+    print("Running MNA... ")
 
     design_name = macromodel.name
     # Generation of the .spice from the schematic.
@@ -19,12 +21,10 @@ def mna(XSCHEM_RCFILE, XSCHEM_DIR, SPICE_DIR, OUTPUT_DIR, macromodel):
             XSCHEM_RCFILE,
             "-o",
             SPICE_DIR,
-            XSCHEM_DIR
-            + design_name
-            + ".sch",
+            XSCHEM_DIR + design_name + ".sch",
         ]
     )
-    
+
     # the input netlist of the mna must be plain netlist and not spice, here the convertion is made.
     nodes = spice2ptxt(SPICE_DIR, OUTPUT_DIR, design_name)
 
@@ -35,8 +35,9 @@ def mna(XSCHEM_RCFILE, XSCHEM_DIR, SPICE_DIR, OUTPUT_DIR, macromodel):
     report, df, df2, A, X, Z = symmna.smna(content)
 
     macromodel.A, macromodel.X, macromodel.Z, macromodel.nodes = A, X, Z, nodes
-    
+
     return report, df, df2, A, X, Z, nodes
+
 
 def mna_solve(macromodel):
     eq = sym.Eq(macromodel.A * sym.Matrix(macromodel.X), sym.Matrix(macromodel.Z))
@@ -46,17 +47,19 @@ def mna_solve(macromodel):
 
     return sol
 
+
 def mna_tf(macromodel):
-    
     sol = macromodel.eq_solutions
     nodes = macromodel.nodes
     req_tfs = macromodel.req_tfs
 
     tfs = []
     for idx, req_tf in enumerate(req_tfs):
-        tfs.append(sol[sym.sympify("v"+str(nodes[req_tf[0]].iloc[0]))]
-                   /sol[sym.sympify("v"+str(nodes[req_tf[1]].iloc[0]))])
-        
-    macromodel.tfs_sol = tfs    
-        
+        tfs.append(
+            sol[sym.sympify("v" + str(nodes[req_tf[0]].iloc[0]))]
+            / sol[sym.sympify("v" + str(nodes[req_tf[1]].iloc[0]))]
+        )
+
+    macromodel.tfs_sol = tfs
+
     return tfs
