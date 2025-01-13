c8e42b7e0febed69868bc2d66dc450309abb34d3
Daniel Arevalos <daniel.arevalos@sansano.usm.cl>
2025-01-13 18:09:36 +0100

    update

--- sstadex/__init__.py
+++ sstadex/__init__.py
@@ -1,7 +1,19 @@
 from .mna import mna, mna_solve, mna_tf  # Import the function directly
 from .utils import spice2ptxt
-from .models import Macromodel, Primitive, Transistor
-from .sstadex import topdown_prim_lookup, bfs
+from .models import Macromodel, Primitive, Transistor, simplediffpair, cs_pmos, cm_pmos
+from .sstadex import topdown_prim_lookup, bfs, dfs
 
-__all__ = ["mna, spice2ptxt", "Macromodel", "topdown_prim_lookup", "bfs", "mna_solve"
-           , "mna_tf", "Transistor", "Primitive"]
+__all__ = [
+    "mna, spice2ptxt",
+    "Macromodel",
+    "topdown_prim_lookup",
+    "bfs",
+    "dfs",
+    "mna_solve",
+    "mna_tf",
+    "Transistor",
+    "Primitive",
+    "simplediffpair",
+    "cs_pmos",
+    "cm_pmos",
+]
