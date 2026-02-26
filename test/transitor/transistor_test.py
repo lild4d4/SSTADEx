b5f5693a9123264c2d47d6ec5828ed1f1aac2c94
lild4d4 <daniel.arevalos@hm.edu>
2026-02-26 15:19:46 +0000

    added pmos transistor test in the script

--- test/transitor/transistor_test.py
+++ test/transitor/transistor_test.py
@@ -18,3 +18,21 @@ pt_transistor = Transistor(
 )
 
 print(pt_transistor.gmid)
+
+################################################################
+
+vds_pmos = (-1.2, -0.1, 0.1)
+vgs_pmos = (-1.2, -0.1, 0.1)
+
+pt_transistor_pmos = Transistor(
+            lookup_table_file=lookup_table,
+            mos_type="sg13_lv_pmos",
+            vsb=0,
+            vds=vds_pmos,
+            vgs=vgs_pmos,
+            lengths=lengths,
+            dof=['vds', 'vgs'],
+            dof_values=[vds_pmos,vgs_pmos]
+)
+
+print(pt_transistor_pmos.gmid)
