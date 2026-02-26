c66b6cf1a0411e2e5071d22411b28a8dae07f4aa
lild4d4 <daniel.arevalos@hm.edu>
2026-02-26 15:22:54 +0000

    primitive lookup with arrays

--- test/primitive_lookup/primitive_test.py
+++ test/primitive_lookup/primitive_test.py
@@ -5,7 +5,7 @@ lookup_table = "../../LUTs/ihp-sg13g2/lv_10w.npz"
 
 lengths_nmos = [0.4e-06]
 
-vs = np.linspace(0, 0.8, 100)
+vs = np.linspace(0, 0.8, 10)
 vds_diffpar = 1.1 - vs
 vgs_diffpar = 0.9 - vs
 I_amp1 = 5e-6
@@ -17,13 +17,13 @@ diffpair = simplediffpair(
     inputs = { 
         'vds_lut': (0.1, 1.1, 0.01),
         'vgs_lut': (0.1, 1.1, 0.01), 
-        'vds': (0.1, 1, 0.1), 
-        'vgs': (0.1, 1, 0.1), 
+        'vds': vds_diffpar, 
+        'vgs': vgs_diffpar, 
         'il': I_amp1/2,
         'length': lengths_nmos, 
         '2d_var': ['vds', 'vgs']}
 )
 
 diffpair=diffpair.build()
-print(diffpair)
+diffpair.to_csv("diffpair.csv")
 
