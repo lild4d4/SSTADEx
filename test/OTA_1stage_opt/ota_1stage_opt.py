2d06da903c59941ac4d89797cc70f6cf8f8f4f3a
lild4d4 <daniel.arevalos@hm.edu>
2026-04-07 17:34:14 +0000

    add current source macro in ota example

--- test/OTA_1stage_opt/ota_1stage_opt.py
+++ test/OTA_1stage_opt/ota_1stage_opt.py
@@ -44,7 +44,7 @@ gain_condition = 80
 estability_condition = 60
 size_condition = 1e-3
 
-I_bias = 5e-6
+I_bias_ref = 5e-6
 I_amp = 20e-6 
 
 Iq_max = IL*(1-efficiency)
@@ -133,6 +133,39 @@ currentmirror.outputs = {
 
 currentmirror_df.to_csv('currentmirror.csv')
 
+currentsource = lib.get('simplecurrentsource', il=I_amp)
+print(currentsource)
+
+currentsource.set_port_voltages({
+    "VOUTP": vs,
+    "VSS": 0,
+    "VINP": vs, 
+})
+print(currentsource.summary())
+
+currentsource_df = currentsource.build(ref_current=I_amp)
+
+print("\n--- DataFrame ---")
+print(f"shape: {currentsource_df.shape}")
+print(currentsource_df.head())
+
+
+currentsource_mask = (currentsource_df["width_m1"]>1e-6) & (currentsource_df["width_m2"]>1e-6)
+currentsource_df = currentsource_df[currentsource_mask]
+
+currentsource.parameters = {
+    Symbol('g_gm_cs_m1'): currentsource_df['gm'].values,
+    Symbol('R_gds_cs_m1'): currentsource_df['Ro'].values
+}
+
+currentsource.outputs = {
+    Symbol("W_cs_m1"): currentsource_df["width_m1"].values,
+    Symbol("W_cs_m2"): currentsource_df["width_m2"].values,
+    Symbol("L_cs"): currentsource_df["length"].values,
+}
+
+currentsource_df.to_csv('currentsource.csv')
+
 ########################## MACROMODELS ############################################
 
 # OTA_1stage_macro 
@@ -186,6 +219,55 @@ OTA_1stage_macro.add_instance(
     },
 )
 
+current_source_macro = Macromodel(
+    name = 'current_source_macro',
+    ports = ['VOUT', 'VSS'],
+    outputs = [
+        Symbol("W_cs_m1"),
+        Symbol("W_cs_m2"),
+        Symbol("L_cs"),
+    ],
+    electrical_parameters = {
+        'Iref': I_bias_ref,
+        'Ibias': I_amp,
+        'Vdd': Vin,
+    },
+    model="""I{INSTANCE} {VOUT} {VSS} 0""",
+    macromodel_parameters = {
+        Symbol('Ics'): [I_amp]
+    }
+)
+
+OTA_1stage_macro.add_instance(
+    "cs",
+    current_source_macro,
+    {
+        "VOUT": "IBIAS",
+        "VSS": "VSS"
+    },
+    index=0,
+    netlist_params={
+        'Ibias': Symbol('Ibias')
+    }
+)
+
+current_source_macro.add_instance(
+    "cs",
+    currentsource,
+    {
+        "VOUTP": "VOUT",
+        "VSS": "VSS", 
+        "VOUTN": "Vbias",
+        "VINP": "Vbias"
+    },
+    index=0,
+    netlist_params={
+        "W_cs_m1": Symbol("W_cs_m1"),
+        "W_cs_m2": Symbol("W_cs_m2"),
+        "L_cs": Symbol("L_cs"),
+    }
+)
+
 #################### TESTBENCHES ##########################
 
 tb_gain = Testbench(
@@ -193,7 +275,6 @@ tb_gain = Testbench(
     dut=OTA_1stage_macro,
     view="small_signal",
     elements=[
-        CurrentSource("I2", "IBIAS", "VSS", 0),
         VoltageSource("Vdd", "VDD", "VSS", 0),
         VoltageSource("Vss", "VSS", "VSS", 0),
         VoltageSource("V_n", "VINN", "VSS", 0),
@@ -205,7 +286,6 @@ tb_gain = Testbench(
         Symbol("Vss"): 0,
         Symbol("V_n"): 0,
         Symbol("V_p"): 1,
-        Symbol("I2"): 0,
         Symbol("g_gm_xdp_m2"): Symbol("g_gm_xdp_m1"),
         Symbol("R_gds_xdp_m2"): Symbol("R_gds_xdp_m1"),
         Symbol("g_gm_xcm_m2"): Symbol("g_gm_xcm_m1"),
@@ -225,7 +305,6 @@ tb_rout = Testbench(
     dut=OTA_1stage_macro,
     view="small_signal",
     elements=[
-        CurrentSource("I2", "IBIAS", "VSS", 0),
         VoltageSource("Vdd", "VDD", "VSS", 0),
         VoltageSource("Vss", "VSS", "VSS", 0),
         VoltageSource("V_n", "VINN", "VSS", 0),
@@ -240,7 +319,6 @@ tb_rout = Testbench(
         Symbol("V_n"): 0,
         Symbol("V_p"): 0,
         Symbol("Vr"): 1,
-        Symbol("I2"): 0,
         Symbol("Rr"): 1000,
         Symbol("g_gm_xdp_m2"): Symbol("g_gm_xdp_m1"),
         Symbol("R_gds_xdp_m2"): Symbol("R_gds_xdp_m1"),
@@ -257,13 +335,44 @@ rout_1stage = tb_rout.make_test(
     lamd=lambda x: x * 1000 / (1 - x),
 )
 
+tb_ibias = Testbench(
+    name="currentsource_ibas",
+    dut=current_source_macro,
+    view="small_signal",
+    elements=[
+        VoltageSource("Vdd", "VDD", "VSS", 0),
+        VoltageSource("Vss", "VSS", "VSS", 0),
+        CurrentSource("I0", "Vbias", "VSS", 0),
+    ],
+    tf=("VOUT", "Vbias"),
+    parameter_map={
+        Symbol("Vdd"): 0,
+        Symbol("Vss"): 0,
+        Symbol("I0"): 0,
+        Symbol("s"): 0,
+        Symbol('g_gm_cs_m2'): Symbol('g_gm_cs_m1'),
+        Symbol('R_gds_cs_m2'): Symbol('R_gds_cs_m1')
+    }
+)
+
+ibias_currentsource = tb_ibias.make_test(
+    name="ibias_currentsource",
+    opt_goal="max",
+    conditions={"min": [0]},
+)
+
+current_source_macro.specifications=[ibias_currentsource]
+current_source_macro.primitives = [currentsource]
+current_source_macro.num_level_exp = 1
+current_source_macro.run_pareto = False
+
 OTA_1stage_macro.specifications = [gain_1stage, rout_1stage]
 OTA_1stage_macro.opt_specifications = [gain_1stage]
 OTA_1stage_macro.primitives = [diffpair, currentmirror]
-OTA_1stage_macro.submacromodels = []
+OTA_1stage_macro.submacromodels = [current_source_macro]
 OTA_1stage_macro.num_level_exp = -1
 OTA_1stage_macro.is_primitive = 0
-OTA_1stage_macro.run_pareto = True
+OTA_1stage_macro.run_pareto = False
 
 _, _, _, ota_1stage_df, mask = dfs(OTA_1stage_macro, debug = False)
 
@@ -283,67 +392,67 @@ ota_1stage_df_filtered.to_csv('ota_1stage_df_filtered.csv')
 
 print("w_diff", ota_1stage_df_filtered[Symbol("W_diff")].to_numpy())
 
-point = {
-    Symbol("W_diff"): ota_1stage_df_filtered[Symbol("W_diff")].to_numpy(),
-    Symbol("L_diff"): ota_1stage_df_filtered[Symbol("L_diff")].to_numpy(),
-    Symbol("W_al"): ota_1stage_df_filtered[Symbol("W_al")].to_numpy(),
-    Symbol("L_al"): ota_1stage_df_filtered[Symbol("L_al")].to_numpy(),
-}
+# point = {
+#     Symbol("W_diff"): ota_1stage_df_filtered[Symbol("W_diff")].to_numpy(),
+#     Symbol("L_diff"): ota_1stage_df_filtered[Symbol("L_diff")].to_numpy(),
+#     Symbol("W_al"): ota_1stage_df_filtered[Symbol("W_al")].to_numpy(),
+#     Symbol("L_al"): ota_1stage_df_filtered[Symbol("L_al")].to_numpy(),
+# }
 
-simulations = OTA_1stage_macro.ngspice_sim(
-    point,
-    variables=["gain", "vout"],
-    extra_spice = {
-        "pre": [
-            "**",
-            "x1 net1 vn vout vdd ibias OTA_1stage_macro", # VINP VINN VOUTP VDD IBIAS
-            "R1 vfb vout 100000000 m=1",
-            "C2 vfb vss 10 m=1",
-            "R2 vfb vss 900000000 m=1",
-            "V1 net1 vfb dc 0 ac 1",
-            "I1 ibias vss 20e-6",
-            ".lib /home/designer/shared/SSTADEx/IHP-Open-PDK/ihp-sg13g2/libs.tech/ngspice/models/cornerMOSlv.lib mos_tt",
-            "Vref vn 0 0.9",
-            "Vdd vdd 0 1.5",
-            "Vss vss 0 0",
-            ".control",
-            "ac dec 10 1 1G",
-            "meas ac gain find vdb(vout) at=1000",
-            "op",
-            "print v(vout)",
-            ".endc",
-            ".end"
-        ]
-    })
+# simulations = OTA_1stage_macro.ngspice_sim(
+#     point,
+#     variables=["gain", "vout"],
+#     extra_spice = {
+#         "pre": [
+#             "**",
+#             "x1 net1 vn vout vdd ibias OTA_1stage_macro", # VINP VINN VOUTP VDD IBIAS
+#             "R1 vfb vout 100000000 m=1",
+#             "C2 vfb vss 10 m=1",
+#             "R2 vfb vss 900000000 m=1",
+#             "V1 net1 vfb dc 0 ac 1",
+#             "I1 ibias vss 20e-6",
+#             ".lib /home/designer/shared/SSTADEx/IHP-Open-PDK/ihp-sg13g2/libs.tech/ngspice/models/cornerMOSlv.lib mos_tt",
+#             "Vref vn 0 0.9",
+#             "Vdd vdd 0 1.5",
+#             "Vss vss 0 0",
+#             ".control",
+#             "ac dec 10 1 1G",
+#             "meas ac gain find vdb(vout) at=1000",
+#             "op",
+#             "print v(vout)",
+#             ".endc",
+#             ".end"
+#         ]
+#     })
 
-print(simulations)
+# print(simulations)
 
-sim_results_df = pd.DataFrame(
-    [
-        {
-            **sim_result["params"],
-            **{
-                f"sim_{name}": value
-                for name, value in sim_result.get("variables", {}).items()
-            },
-            "run_name": sim_result["run_name"],
-            "returncode": sim_result["returncode"],
-        }
-        for sim_result in simulations
-    ]
-)
+# sim_results_df = pd.DataFrame(
+#     [
+#         {
+#             **sim_result["params"],
+#             **{
+#                 f"sim_{name}": value
+#                 for name, value in sim_result.get("variables", {}).items()
+#             },
+#             "run_name": sim_result["run_name"],
+#             "returncode": sim_result["returncode"],
+#         }
+#         for sim_result in simulations
+#     ]
+# )
 
-ota_1stage_comparison_df = pd.concat(
-    [
-        ota_1stage_df_filtered.reset_index(drop=True),
-        sim_results_df[
-            ["run_name", "returncode", "sim_gain", "sim_vout"]
-        ].reset_index(drop=True),
-    ],
-    axis=1,
-)
+# ota_1stage_comparison_df = pd.concat(
+#     [
+#         ota_1stage_df_filtered.reset_index(drop=True),
+#         sim_results_df[
+#             ["run_name", "returncode", "sim_gain", "sim_vout"]
+#         ].reset_index(drop=True),
+#     ],
+#     axis=1,
+# )
 
-ota_1stage_comparison_df["error"] = np.abs(ota_1stage_comparison_df["gain"] - ota_1stage_comparison_df["sim_gain"])
+# ota_1stage_comparison_df["error"] = np.abs(ota_1stage_comparison_df["gain"] - ota_1stage_comparison_df["sim_gain"])
 
-ota_1stage_comparison_df.to_csv("ota_1stage_comparison.csv", index=False)
-print(ota_1stage_comparison_df)
+# ota_1stage_comparison_df.to_csv("ota_1stage_comparison.csv", index=False)
+# print(ota_1stage_comparison_df)
