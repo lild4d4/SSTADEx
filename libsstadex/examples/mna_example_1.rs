a2e3a2b9111e062ac4c56b44d28611885940947c
lild4d4 <daniel.arevalos@hm.edu>
2026-06-10 15:02:31 +0200

    added mna examples

---
+++ libsstadex/examples/mna_example_1.rs
@@ -0,0 +1,30 @@
+use std::fs;
+use std::path::Path;
+
+use libsstadex::mna::mna::{mna, mna_solve};
+use libsstadex::mna::pretty::{pretty_system, pretty_solutions};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let spice_dir = Path::new(env!("CARGO_MANIFEST_DIR")).join("examples/spice");
+    let output_dir = std::env::temp_dir().join("mna_example_output");
+
+    fs::create_dir_all(&output_dir)?;
+
+    let mna_sys = mna(&spice_dir, &output_dir, "ota").unwrap();
+
+    println!();
+    println!("MNA report");
+    println!("==========");
+    println!("{}", mna_sys.report);
+    
+    println!();
+    println!("{}", pretty_system(&mna_sys.a, &mna_sys.x, &mna_sys.z));
+
+    let mna_sol = mna_solve(&mna_sys.a, &mna_sys.x, &mna_sys.z).unwrap();
+
+    println!("{}", pretty_solutions(&mna_sol.solutions));
+
+    let _ = fs::remove_dir_all(&output_dir);
+
+    Ok(())
+}
