c75915986ca23c24b294a6c5a7cacce97228b20e
lild4d4 <daniel.arevalos@hm.edu>
2026-06-10 09:50:13 +0200

    add symmna_test and spice for tests

---
+++ libsstadex/src/tests/mna_tests/symmna_test.rs
@@ -0,0 +1,18 @@
+use crate::mna::symmna::smna;
+use std::fs;
+use std::path::Path;
+
+#[test]
+fn test_symmna() {
+    
+    let netlist = Path::new("src/tests/netlist/ota.cir");
+    let output_dir = std::env::temp_dir().join("symmna_output");
+
+    let content = fs::read_to_string(netlist).unwrap();
+
+    fs::create_dir_all(&output_dir).unwrap();
+
+    let mna_map = smna(&content).unwrap();
+
+    println!("{}", mna_map.report);
+}
