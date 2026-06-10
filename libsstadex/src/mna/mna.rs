03297b46fbb7ca1d2d2edb2262ee6832e6b287b2
lild4d4 <daniel.arevalos@hm.edu>
2026-06-10 09:42:27 +0200

    add the MnaResult struct, change MnaError types and remove macromodel use inside the function

--- libsstadex/src/mna/mna.rs
+++ libsstadex/src/mna/mna.rs
@@ -1,15 +1,22 @@
 use std::fs;
-use std::Path;
-use std::process::Command;
+use std::path::Path;
 
-use crate::macromodel::Macromodel;
-use crate::spice::converter::spice2ptxt
+use crate::mna::spice_parser::{spice_parser, NodeMap, SpiceError};
+use crate::mna::symmna::{smna, Matrix, Vector, SmnaError};
+
+pub struct MnaResult {
+      pub report: String,
+      pub a: Matrix,
+      pub x: Vector,
+      pub z: Vector,
+      pub nodes: NodeMap,
+  }
 
 #[derive(Debug)]
 pub enum MnaError {
     Io(std::io::Error),
-    SpiceConversion(String),
-    SymMna(String),
+    SpiceConversion(SpiceError),
+    SymMna(SmnaError),
     MissingNode(String),
 }
 
@@ -20,31 +27,20 @@ impl From<std::io::Error> for MnaError {
 }
 
 pub fn mna(
-    xschem_rcfile: &Path,
-    xscehm_dir: &Path,
     spice_dir: &Path,
     output_dir: &Path,
-    macromodel: &mut Macromodel,
+    design_name: &str,
     ) -> Result<MnaResult, MnaError> {
     println!("Running MNA...");
 
-    let design_name = &macromodel.name;
-
-    let nodes = spice2ptxt(spice_dir, output_dir, design_name).map_err(MnaError::SpiceConversion)?;
-    let input_path = output_dir.join(format!("{design_name}.txt"));
+    let nodes = spice_parser(spice_dir, output_dir, design_name).map_err(MnaError::SpiceConversion)?;
+    let input_path = output_dir.join(format!("{design_name}.cir"));
     let content = fs::read_to_string(input_path)?;
 
     let symmna_output = smna(&content).map_err(MnaError::SymMna)?;
 
-    macromodel.a = Some(symmna_output.a.clone());
-    macromodel.x = Some(symmna_output.x.clone());
-    macromodel.z = Some(symmna_output.z.clone());
-    macromodel.nodes = Some(nodes.clone());
-
     Ok(MnaResult {
         report: symmna_output.report,
-        df: symmna_output.df,
-        df2: symna_output.df2,
         a: symmna_output.a,
         x: symmna_output.x,
         z: symmna_output.z,
